6eec60657b835f57cf263ce49f70975c1474caf8
dursunnguven <dursunnguvenn@gmail.com>
2023-10-29 11:17:29 +0300

    Sayi-Tahmin-Oyunu

---
+++ SayiTahmin/SayiTahmin/SayiTahmin.cpp
@@ -0,0 +1,41 @@
+﻿/*SAYI TAHMİN OYUNU*/
+
+#include <iostream>
+#include <stdlib.h>  //random sayı üretmeyi sağlayan kütüphane
+#include <ctime>	//pc zamanı ile programın zamanını senkronize eden kütüphane
+
+using namespace std;
+
+int main(){
+	int sayi;
+	char cevap ='e';
+	srand(time(NULL));
+	cout << "SAYI TAHMIN OYUNU" << endl << endl;
+	while (cevap=='e')
+	{
+		int sayi2 = rand() % 100; //0dan 100e kadar rastgele sayı üretir
+		cout << "Bir sayi tuttum hadi tahmin et:)" << endl;
+		cin >> sayi;
+
+		while (sayi != sayi2) {
+			if (sayi>sayi2)
+			{
+				cout << "daha kucuk bir sayi gir." << endl;
+			}
+			else
+			{
+				cout << "daha buyuk bir sayi gir." << endl;
+			}
+			cin >> sayi;
+		}
+		cout << "Tahminin Dogru.Tebrikler!!!" << endl;
+		cout << "Devam Etmek Ister Misin?(e veya h)" << endl;
+		cin >> cevap;
+
+	}
+
+
+
+
+
+}
